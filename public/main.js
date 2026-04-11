200e31e8f146a0282b8a3d1bec5bd21ac0e87d8c
Dawood Sardar <dawoodsardar252@gmail.com>
2026-04-11 07:51:21 +0500

    Add pending message status icon to message elements

--- public/main.js
+++ public/main.js
@@ -20,7 +20,6 @@ form.addEventListener("submit", (e) => {
   message.value = "";
 });
 
-// main.js - Update this function
 function createMessageElement(msg) {
   const li = document.createElement("li");
 
@@ -36,8 +35,20 @@ function createMessageElement(msg) {
   li.classList.add(userClass);
 
   const textSpan = document.createElement("span");
-
   textSpan.textContent = `${msg.sender}: ${msg.text}`;
+
+  // Add clock icon if it is pending
+
+  if ((msg.status = "pending")) {
+    const statusIcon = document.createElement("span");
+    statusIcon.classList.add("status-icon");
+    statusIcon.innerHTML = `<svg 
+    xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#">
+    <path d="M320-160h320v-120q0-66-47-113t-113-47q-66 0-113 47t-47 113v120Zm273-407q47-47 47-113v-120H320v120q0 66 47 113t113 47q66 0 113-47ZM160-80v-80h80v-120q0-61 28.5-114.5T348-480q-51-32-79.5-85.5T240-680v-120h-80v-80h640v80h-80v120q0 61-28.5 114.5T612-480q51 32 79.5 85.5T720-280v120h80v80H160Zm320-80Zm0-640Z"/>
+    </svg>`;
+
+    li.appendChild(statusIcon);
+  }
 
   const timeSpan = document.createElement("span");
   timeSpan.classList.add("timestamp");
