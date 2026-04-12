758a6f51696ebd23d219b18fc661dad34b4c56fe
Dawood Sardar <dawoodsardar252@gmail.com>
2026-04-12 10:18:34 +0500

    Update sent message styles for better alignment and enhance status icon SVG

--- public/main.js
+++ public/main.js
@@ -70,7 +70,7 @@ function createMessageElement(msg) {
   if (msg.status === "pending") {
     const statusIcon = document.createElement("span");
     statusIcon.classList.add("status-icon");
-    statusIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#"><path d="M127.5-267.5Q40-355 40-480t87.5-212.5Q215-780 340-780t212.5 87.5Q640-605 640-480t-87.5 212.5Q465-180 340-180t-212.5-87.5ZM740-160v-488l-44 44-56-56 140-140 140 140-57 56-43-43v487h-80ZM496-324q64-64 64-156t-64-156q-64-64-156-64t-156 64q-64 64-64 156t64 156q64 64 156 64t156-64Zm-76-16 56-56-96-97v-147h-80v180l120 120Zm-80-140Z"/></svg>`;
+    statusIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M600-160q-134 0-227-93t-93-227q0-133 93-226.5T600-800q133 0 226.5 93.5T920-480q0 134-93.5 227T600-160Zm0-80q100 0 170-70t70-170q0-100-70-170t-170-70q-100 0-170 70t-70 170q0 100 70 170t170 70Zm91-91 57-57-108-108v-144h-80v177l131 132ZM80-600v-80h160v80H80ZM40-440v-80h200v80H40Zm40 160v-80h160v80H80Zm520-200Z"/></svg>`;
     // Delay showing icon
     const timeout = setTimeout(() => {
       li.appendChild(statusIcon);
